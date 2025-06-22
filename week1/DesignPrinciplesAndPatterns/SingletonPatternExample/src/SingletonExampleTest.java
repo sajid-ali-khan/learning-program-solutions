a4d91fd99e362faa6ee62e8f1b77189066a5ec81
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-06-22 19:19:59 +0530

    organize files

--- week1/DesignPrinciplesAndPatterns/SingletonPatternExample/src/SingletonExampleTest.java
+++ week1/DesignPrinciplesAndPatterns/SingletonPatternExample/src/SingletonExampleTest.java
@@ -1,11 +1,16 @@
-public class Main {
+public class SingletonExampleTest {
     public static void main(String[] args) {
         Logger logger1 = Logger.getInstance();
         Logger logger2 = Logger.getInstance();
 
         System.out.println("logger1 = "+logger1);
         System.out.println("logger2 = "+logger2);
-        System.out.println(logger1 == logger2);
+
+        if(logger1 == logger2){
+            System.out.println("Both logger instances are same.");
+        }else{
+            System.out.println("The logger instances are not same.");
+        }
 
         logger1.log("Main", "End of execution.");
     }

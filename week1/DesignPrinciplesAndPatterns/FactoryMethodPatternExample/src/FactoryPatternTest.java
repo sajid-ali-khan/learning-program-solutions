c953cd225bfbc4440d2d3cae3ff9186b89d7df63
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-06-22 23:08:39 +0530

    add output file

--- week1/DesignPrinciplesAndPatterns/FactoryMethodPatternExample/src/FactoryPatternTest.java
+++ week1/DesignPrinciplesAndPatterns/FactoryMethodPatternExample/src/FactoryPatternTest.java
@@ -2,7 +2,7 @@ import documents.IDocument;
 import factories.DocumentFactory;
 import factories.PdfDocumentFactory;
 
-public class Main {
+public class FactoryPatternTest {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         DocumentFactory factory = new PdfDocumentFactory();
         IDocument document = factory.createDocument();

a9ba098f35ab514b3cb1041b678f58ae76c25fe7
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-06-27 18:00:32 +0530

    add JunitAssertions exercise

---
+++ week2/JUnit5_Mockito/JunitAssertions/src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int divide(int numerator, int denominator) {return numerator / denominator;}
+}
---
+++ week2/JUnit5_Mockito/JunitAssertions/src/test/java/CalculatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CalculatorTest {
+    static Calculator calculator;
+
+    @BeforeAll
+    static void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    void testAdd(){
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+    void testDivide(){
+        assertEquals(1, calculator.divide(2, 2));
+        assertThrows(ArithmeticException.class, () -> calculator.divide(2, 0));
+        assertNotEquals(2, calculator.divide(2, 2));
+    }
+}

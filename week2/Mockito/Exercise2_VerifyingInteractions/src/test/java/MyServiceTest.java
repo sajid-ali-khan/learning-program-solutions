f24fac1f4c66fe4d720160dff7db20cb2786a723
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-06-27 20:17:37 +0530

    add Exercise2 and test outputs

---
+++ week2/Mockito/Exercise2_VerifyingInteractions/src/test/java/MyServiceTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class MyServiceTest {
+    @Test
+    void fetchDataTest(){
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}

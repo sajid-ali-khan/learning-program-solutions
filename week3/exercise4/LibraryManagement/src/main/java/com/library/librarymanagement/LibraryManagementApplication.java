313bd577f9e212eadbe8bfddaadd6d4ace1471b7
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-07-13 10:06:12 +0530

    add ex4

---
+++ week3/exercise4/LibraryManagement/src/main/java/com/library/librarymanagement/LibraryManagementApplication.java
@@ -0,0 +1,11 @@
+package com.library.librarymanagement;
+
+
+public class LibraryManagementApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ week3/exercise4/LibraryManagement/src/test/java/com/library/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,38 @@
+package com.library.librarymanagement;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LibraryManagementApplicationTests
+		extends TestCase
+{
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public LibraryManagementApplicationTests( String testName )
+	{
+		super( testName );
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite()
+	{
+		return new TestSuite( LibraryManagementApplicationTests.class );
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp()
+	{
+		assertTrue( true );
+	}
+}

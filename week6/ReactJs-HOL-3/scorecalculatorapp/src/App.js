aa711b5ad8d5071b751cd7fb301698575a7a7871
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-08-02 13:07:14 +0530

    add file3

---
+++ week6/ReactJs-HOL-3/scorecalculatorapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { CalculateScore } from './Components/CalculateScores';
+
+function App() {
+  return (
+    <CalculateScore Name={"Steeve"}
+    School={"DNV public school"}
+    total={284}
+    goal={3}
+    />
+  );
+}
+
+export default App;

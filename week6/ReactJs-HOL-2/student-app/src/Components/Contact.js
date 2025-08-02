5f23238650ca1fb7b0c225392c21e188d723eabe
Patan Sajid Ali Khan <sajid@ubuntu.com>
2025-08-02 12:30:33 +0530

    add file 2

---
+++ week6/ReactJs-HOL-2/student-app/src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Welcome to the Abouts page of Student Management Portal.</h3>
+            </div>
+        );
+    }
+}
